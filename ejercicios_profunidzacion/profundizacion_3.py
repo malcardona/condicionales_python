d7e3ff820d3b21246d0f32e76926780cd694de21
Alejandro L. Cardona <acardona@unc.edu.ar>
2021-07-22 18:09:36 -0300

    Update profundizacion_3.py

--- ejercicios_profunidzacion/profundizacion_3.py
+++ ejercicios_profunidzacion/profundizacion_3.py
@@ -30,3 +30,59 @@ contenido. Recomendamos pensar bien este problema de lógica con un lápiz y pap
 
 print('Ejercicios de práctica con números')
 # Empezar aquí la resolución del ejercicio
+
+# Solicitar Temperaturas
+print('Ingrese tres Temperaturas')
+num_1 = int(input('Temperatura 1:\n')) 
+num_2 = int(input('Temperatura 2:\n')) 
+num_3 = int(input('Temperatura 3:\n')) 
+
+# Promedio Temperaturas
+temp_avg = (num_1 + num_2 + num_3) / 3
+
+# Ordenar Temperaturas
+
+if num_1 == num_2 and num_1 == num_3:
+    print('Todas las Temperatutas son iguales')
+
+elif (num_1 == num_2 or num_1 == num_3):
+    if num_2 >  num_3:
+        temp_1 = num_2
+        temp_2 = num_3
+    else:
+        temp_1 = num_3
+        temp_2 = num_2
+
+elif num_2 == num_3:
+    if num_1 >  num_3:
+        temp_1 = num_1
+        temp_2 = num_3
+    else:
+        temp_1 = num_3
+        temp_2 = num_1
+
+else:
+    if num_1 > num_2:
+        if num_1 < num_3:
+            temp_1 = num_3
+            temp_2 = num_2
+        elif num_2 > num_3:
+            temp_1 = num_1
+            temp_2 = num_3
+    elif num_2 > num_3:
+        if num_3 > num_1:
+            temp_1 = num_2
+            temp_2 = num_1
+        else:
+            temp_1 = num_2
+            temp_2 = num_3
+    else:
+        if num_1 > num_3:
+            temp_1 = num_2
+            temp_2 = num_3
+        else:
+            temp_1 = num_2
+            temp_2 = num_1
+
+print('La Temperatura máxima es {}°C y la mínima es {}°C'.format(temp_1, temp_2))
+print('La Temperatura Promedio es {}°C'.format(temp_avg))
