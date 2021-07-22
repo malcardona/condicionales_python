8240c32cc9472de090c2e83640beb8abe2c4a3b3
Alejandro L. Cardona <acardona@unc.edu.ar>
2021-07-22 19:37:09 -0300

    Update profundizacion_4.py

--- ejercicios_profunidzacion/profundizacion_4.py
+++ ejercicios_profunidzacion/profundizacion_4.py
@@ -33,3 +33,83 @@ contenido.
 
 print('Ejercicios de práctica con cadenas')
 # Empezar aquí la resolución del ejercicio
+
+# Solicitar Palabras
+print('Ingrese tres Palabras')
+texto_1 = str(input('Palabra 1:\n')) 
+texto_2 = str(input('Palabra 2:\n')) 
+texto_3 = str(input('Palabra 3:\n')) 
+
+# Consultar como ordenar palabras
+print('Como desea ordernar las palabras?')
+print('1- Orden Alfabético')
+print('2- Cantidad de Palabras')
+
+ord = str(input())
+
+# Comprobar que la opcion ingresada sea correcta
+if (ord != '1' ) and (ord != '2'):
+    print('Debe ingresar una opción válida')
+    ord = str(input())
+
+# Orden Alfabético
+if ord == '1':
+    print('Se ordenaran las palabras en Orden Alfabético')
+
+    # Si todas las Palabras son iguales
+    if texto_1 == texto_2 and texto_2 == texto_3:
+       print('Todas las palabras son iguales')
+    
+    # Si dos palabras son Iguales
+    elif (texto_1 == texto_2 or texto_1 == texto_3):
+        if texto_2 > texto_3:
+            p_1 = texto_2
+            p_2 = texto_1
+            p_3 = texto_3
+        else:
+            p_1 = texto_3
+            p_2 = texto_1
+            p_3 = texto_2
+        print('{} > {} > {}'.format(p_1, p_2, p_3))
+
+    elif texto_2 == texto_3:
+        if texto_1 > texto_3:
+            p_1 = texto_1
+            p_2 = texto_2
+            p_3 = texto_3
+        else:
+            p_1 = texto_3
+            p_2 = texto_2
+            p_2 = texto_1
+        print('{} > {} > {}'.format(p_1, p_2, p_3))
+
+    # Todas las palabras diferentes
+    else:
+        if texto_1 > texto_2:
+            if texto_1 < texto_3:
+                p_1 = texto_3
+                p_2 = texto_1
+                p_3 = texto_2
+            elif texto_2 > texto_3:
+                p_1 = texto_1
+                p_2 = texto_2
+                p_3 = texto_3
+        elif texto_2 > texto_3:
+            if texto_3 > texto_1:
+                p_1 = texto_2
+                p_2 = texto_3
+                p_3 = texto_1
+            else:
+                p_1 = texto_2
+                p_2 = texto_1
+                p_3 = texto_3
+        else:
+            if texto_1 > texto_3:
+                p_1 = texto_2
+                p_2 = texto_1
+                p_2 = texto_3
+            else:
+                p_1 = texto_2
+                p_2 = texto_3
+                p_2 = texto_1
+        print('{} > {} > {}'.format(p_1, p_2, p_3))
